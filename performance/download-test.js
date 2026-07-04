685cc42c17991ab06d1b0d3747a379519a2c716f
Cédric ROURE <roure.cedric83@gmail.com>
2026-07-04 14:11:27 +0200

    test(perf): add k6 download performance test

---
+++ performance/download-test.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '30s',
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<500'],
+  },
+};
+
+const BASE_URL = 'http://localhost:8080';
+const TOKEN = __ENV.DOWNLOAD_TOKEN;
+
+export default function () {
+  const res = http.get(`${BASE_URL}/api/download/${TOKEN}`);
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'body is not empty': (r) => r.body.length > 0,
+  });
+
+  sleep(1);
+}
