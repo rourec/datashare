78de5511e3c4ef334f568ed737edad865c8230f1
Cédric ROURE <roure.cedric83@gmail.com>
2026-07-05 12:12:20 +0200

    feat(frontend): generate application components

---
+++ frontend/src/app/shared/upload-button/upload-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-button',
+  imports: [],
+  templateUrl: './upload-button.html',
+  styleUrl: './upload-button.scss',
+})
+export class UploadButton {}
